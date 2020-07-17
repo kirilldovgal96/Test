6cd4bb8f8d25a61c476e17d99d0a25c77eefa7c9
kirilldovgal96 <68415088+kirilldovgal96@users.noreply.github.com>
2020-07-17 09:11:25 +0300

    Add files via upload

---
+++ SuperApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Linq;
+using Console = Colorful.Console;
+
+namespace SuperApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var chars = "This is SuperApp sample to show your skills in DevOps".ToArray();
+            Console.WriteWithGradient(chars, Color.Orange, Color.Green, 14);
+            Console.ReadLine();
+        }
+    }
+}
